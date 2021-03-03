3543f86ff72653cc0cbe6c72678f31e5dbbd0aad
devggaurav <devggaurav@gmail.com>
2021-03-03 18:10:59 +0530

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+  h1{
+    text-align: center;
+  }
+</style>
